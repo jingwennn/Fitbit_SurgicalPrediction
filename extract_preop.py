8840d65bce2d80db45522f37f11e580a759e5a6c
jingwennn <44712783+jingwennn@users.noreply.github.com>
2022-10-15 17:12:49 -0500

    Add files via upload
    Extract pre-op data

--- extract_preop.py
+++ extract_preop.py
@@ -17,6 +17,7 @@ LABELS_CSV_FILENAME = "ground_truth.csv"
 PREOP_HR_DIR = 'Preop_HR_data/'
 PREOP_STEP_DIR = 'Preop_Step_data/'
 PREOP_SLEEP_DIR = 'Preop_Sleep_data/'
+PREOP_SLEEP_SUMMARY_DIR = 'Preop_Sleep_summary/'
 
 # Some users did not wear the device at the beginning and ending of preop stage
 # Use this funtion to find those missing dates
@@ -62,20 +63,20 @@ if __name__ == "__main__":
             else:
                 break
 
-        hr_transposed_array = np.array(all_preop_hr_data)
+        hr_array = np.array(all_preop_hr_data)
         missing_index = find_missing_index(all_preop_hr_data)
 
         # Remove the completely missing days at the start/end of the preop stage
-        hr_transposed_array = np.delete(hr_transposed_array, missing_index, axis = 0)
+        hr_array = np.delete(hr_array, missing_index, axis = 0)
         # If longer than 30 days, only use the latest observed 30-day data
-        if len(hr_transposed_array)>30:
-            hr_transposed_array = hr_transposed_array[-30:]
+        if len(hr_array)>30:
+            hr_array = hr_array[-30:]
 
         # Save the extracted data
         hr_save_path = PREOP_HR_DIR 
         if not os.path.exists(hr_save_path):
             os.makedirs(hr_save_path)
-        np.savetxt(hr_save_path + ind+'.csv', hr_transposed_array, fmt='%i', delimiter=",")
+        np.savetxt(hr_save_path + ind+'.csv', hr_array, fmt='%i', delimiter=",")
 
 
         # Read Step data
@@ -90,22 +91,22 @@ if __name__ == "__main__":
             else:
                 break
 
-        step_transposed_array = np.array(all_preop_step_data)
+        step_array = np.array(all_preop_step_data)
 
         # Remove the completely missing days at the start/end of the preop stage
-        step_transposed_array = np.delete(step_transposed_array, missing_index, axis = 0)
+        step_array = np.delete(step_array, missing_index, axis = 0)
         # If longer than 30 days, only use the latest observed 30-day data
-        if len(step_transposed_array)>30:
-            step_transposed_array = step_transposed_array[-30:]
+        if len(step_array)>30:
+            step_array = step_array[-30:]
 
         # Save the extracted data
         step_save_path = PREOP_STEP_DIR 
         if not os.path.exists(step_save_path):
             os.makedirs(step_save_path)
-        np.savetxt(step_save_path + ind+'.csv', step_transposed_array, fmt='%i', delimiter=",")
+        np.savetxt(step_save_path + ind+'.csv', step_array, fmt='%i', delimiter=",")
 
 
-        # Read Sleep data
+        # Read Sleep summary data
         # Some users may not have sleep data for the whole study
         if glob.glob(path+'*sleep.csv'):
             sleep_filename = glob.glob(path+'*sleep.csv')[0]
@@ -135,4 +136,27 @@ if __name__ == "__main__":
                 os.makedirs(sleep_save_path)
 
             pd_sleep.to_csv(sleep_save_path+ind+'.csv', index=False, header=False)
+
+
+        # Read Sleep data
+        # Some users may not have sleep data for the whole study
+        if glob.glob(path+'*sleepSummary.csv'):
+            sleep_summary_filename = glob.glob(path+'*sleepSummary.csv')[0]
+            df_sleep_summary = pd.read_csv(sleep_summary_filename, sep=',')
+
+            preop_keep_index = [day_index for day_index in preop_index if day_index not in missing_index]
+            #print(preop_keep_index)
+            if len(preop_keep_index)>30:
+                preop_keep_index = preop_keep_index[-30:]
+
+            all_preop_sleep_data = df_sleep_summary.iloc[preop_keep_index,:]
+
+            # Save sleep summary data
+            sleep_summary_save_path = PREOP_SLEEP_SUMMARY_DIR 
+            if not os.path.exists(sleep_summary_save_path):
+                os.makedirs(sleep_summary_save_path)
+
+            all_preop_sleep_data.to_csv(sleep_summary_save_path+ind+'.csv', index=False)
+
+
 
